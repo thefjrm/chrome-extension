4ae8f8cab70d3ba8fa992ddf21f664e24c50a9ec
thefjrm <felipemartinsplayernet@gmail.com>
2021-11-22 19:06:28 -0300

    Finish the app

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 let myLeads = [];
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
 });
 
-for (let i = 0; i < myLeads.length; i++) {
-  console.log(myLeads[i]);
+deleteBtn.addEventListener("dblclick", function () {
+  myLeads = [];
+  localStorage.clear();
+  render(myLeads);
+});
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `<li>
+      <a target="_blank" href="${leads[i]}">${leads[i]}</a>
+      </li>`;
+  }
+  ulEl.innerHTML = listItems;
 }
